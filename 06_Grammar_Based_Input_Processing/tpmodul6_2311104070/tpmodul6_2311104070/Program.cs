9e1192c78290fc36c5f3de0e6fd04f49cb6a8636
Alvin Bagus Firmansyah <rusmiantofajri@gmail.com>
2025-03-18 18:22:31 +0700

    Menambahkan kelas SayaTubeVideo dengan validasi judul dan penanganan overflow play count

---
+++ 06_Grammar_Based_Input_Processing/tpmodul6_2311104070/tpmodul6_2311104070/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Alvin Bagus Firmansyah");
+            video.PrintVideoDetails();
+            video.IncreasePlayCount(5000);
+            video.PrintVideoDetails();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+        }
+    }
+}
