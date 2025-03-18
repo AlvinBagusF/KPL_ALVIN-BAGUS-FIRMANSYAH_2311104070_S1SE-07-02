5dc4fcaa232e37115adc4120dcf54cdb2c1eb29e
Alvin Bagus Firmansyah <rusmiantofajri@gmail.com>
2025-03-18 14:42:03 +0700

    Menambahkan fitur input nama pengguna dan pengecekan huruf vokal atau konsonan, serta menampilkan array bilangan genap

---
+++ 02_IDE_GUI_Version_Control/modul2_2311104070/modul2_2311104070/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan nama Anda: ");
+        string nama = Console.ReadLine();
+        Console.WriteLine($"Selamat datang, {nama}!");
+
+        Console.Write("Masukkan satu huruf: ");
+        char input = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        if ("AIUEO".Contains(input))
+        {
+            Console.WriteLine($"Huruf {input} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {input} merupakan huruf konsonan");
+        }
+
+        int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+
+        for (int i = 0; i < bilanganGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {bilanganGenap[i]}");
+        }
+    }
+}
