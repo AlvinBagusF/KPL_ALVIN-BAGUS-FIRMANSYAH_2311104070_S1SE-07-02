21273703c4dbf4e2d9f60fe35a45614b9b713d6c
Alvin Bagus Firmansyah <rusmiantofajri@gmail.com>
2025-04-11 11:49:05 +0700

    Fix: ubah TP dari submodule menjadi folder biasa

---
+++ 07_Runtime_Configuration_Internationalization/TP/tpmodul7_kelompok_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using tpmodul7_kelompok;
+
+namespace tpmodul7_kelompok_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KuliahMahasiswa2311104070.ReadJSON();
+            DataMahasiswa2311104070.ReadJSON();
+        }
+    }
+}
