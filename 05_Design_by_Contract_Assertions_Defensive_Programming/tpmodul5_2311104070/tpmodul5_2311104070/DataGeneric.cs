e6e2f816469e8d67a991758adb4d05b9da5ec068
Alvin Bagus Firmansyah <rusmiantofajri@gmail.com>
2025-04-15 11:21:06 +0700

    Implementasi lengkap Jurnal Modul 5: Penjumlahan dan SimpleDataBase

--- 05_Design_by_Contract_Assertions_Defensive_Programming/tpmodul5_2311104070/tpmodul5_2311104070/DataGeneric.cs
+++ 05_Design_by_Contract_Assertions_Defensive_Programming/tpmodul5_2311104070/tpmodul5_2311104070/DataGeneric.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿    using System;
 
-class DataGeneric<T>
-{
-    private T data;
-
-    public DataGeneric(T data)
+    class DataGeneric<T>
     {
-        this.data = data;
-    }
+        private T data;
 
-    public void PrintData()
-    {
-        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        }
     }
-}
