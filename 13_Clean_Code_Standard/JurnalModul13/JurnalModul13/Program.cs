45e418c8660603ee8fb1094179f6504cc2ef6bb1
Alvin Bagus Firmansyah <rusmiantofajri@gmail.com>
2025-06-07 06:25:58 +0700

    tp 13 dan jm 13

---
+++ 13_Clean_Code_Standard/JurnalModul13/JurnalModul13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var data1 = PusatDataSingleton.GetDataSingleton();
+        var data2 = PusatDataSingleton.GetDataSingleton();
+
+        data1.AddSebuahData("Alvin");
+        data1.AddSebuahData("Davina");
+        data1.AddSebuahData("Freya");
+
+        Console.WriteLine("Data dari data2:");
+        data2.PrintSemuaData();
+
+        data2.HapusSebuahData(2);
+
+        Console.WriteLine("\nSetelah Freya dihapus dari data2, cetak dari data1:");
+        data1.PrintSemuaData();
+
+        Console.WriteLine($"\nJumlah data di data1: {data1.GetSemuaData().Count}");
+        Console.WriteLine($"Jumlah data di data2: {data2.GetSemuaData().Count}");
+    }
+}
---
+++ 13_Clean_Code_Standard/TPModul13/TPModul13/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Interface Observer
+public interface IObserver
+{
+    void Update(string message);
+}
+
+// Interface Subject
+public interface ISubject
+{
+    void Attach(IObserver observer);
+    void Detach(IObserver observer);
+    void Notify(string message);
+}
+
+// Concrete Subject
+public class NewsAgency : ISubject
+{
+    private List<IObserver> observers = new List<IObserver>();
+
+    public void Attach(IObserver observer) => observers.Add(observer);
+    public void Detach(IObserver observer) => observers.Remove(observer);
+
+    public void Notify(string message)
+    {
+        foreach (var observer in observers)
+            observer.Update(message);
+    }
+
+    public void AddNews(string news)
+    {
+        Console.WriteLine("News Added: " + news);
+        Notify(news);
+    }
+}
+
+// Concrete Observer
+public class NewsReader : IObserver
+{
+    private string _name;
+
+    public NewsReader(string name)
+    {
+        _name = name;
+    }
+
+    public void Update(string message)
+    {
+        Console.WriteLine($"{_name} received news: {message}");
+    }
+}
+
+// Main Program
+class Program
+{
+    static void Main()
+    {
+        var agency = new NewsAgency();
+
+        var reader1 = new NewsReader("Alvin");
+        var reader2 = new NewsReader("Davina");
+
+        agency.Attach(reader1);
+        agency.Attach(reader2);
+
+        agency.AddNews("Design Pattern Observer Telah Dipahami!");
+    }
+}
