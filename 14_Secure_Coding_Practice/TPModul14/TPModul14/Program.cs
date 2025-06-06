2da2ec4a269c771d9add926886a2c47010decb64
Alvin Bagus Firmansyah <rusmiantofajri@gmail.com>
2025-06-07 06:28:40 +0700

    tpmodul 14

---
+++ 14_Secure_Coding_Practice/TPModul14/TPModul14/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Review Film oleh Alvin Bagus Firmansyah");
+
+        for (int i = 0; i < 10; i++)
+        {
+            video.IncreasePlayCount(1000000); // Total akan jadi 10 juta play count
+        }
+
+        video.PrintVideoDetails();
+    }
+}
---
+++ 14_Secure_Coding_Practice/TPModul14/TPModul14/SayaTubeVideo.cs
@@ -0,0 +1,42 @@
+﻿public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        if (string.IsNullOrEmpty(title) || title.Length > 100)
+            throw new ArgumentException("Judul harus 1 - 100 karakter.");
+
+        Random random = new Random();
+        id = random.Next(10000, 99999);
+        this.title = title;
+        playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count < 0 || count > 10000000)
+            throw new ArgumentOutOfRangeException("Count harus antara 0 - 10.000.000.");
+
+        try
+        {
+            checked
+            {
+                playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Overflow terjadi saat menambahkan play count.");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+    }
+}
