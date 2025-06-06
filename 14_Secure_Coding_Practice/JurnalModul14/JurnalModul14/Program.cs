fcd6152f7a772e6af3e62d99308f57ede1e77632
Alvin Bagus Firmansyah <rusmiantofajri@gmail.com>
2025-06-07 06:37:43 +0700

    menambahkan JurnalModul 14

---
+++ 14_Secure_Coding_Practice/JurnalModul14/JurnalModul14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Review Film oleh Alvin Bagus Firmansyah");
+
+        for (int i = 0; i < 10; i++)
+        {
+            video.IncreasePlayCount(1000000);
+        }
+
+        video.PrintVideoDetails();
+    }
+}
